a0824d2194ef9151a6f12e2e03d6278f4854f0ae
Daniel Kopecek <xkopecek@fi.muni.cz>
2011-05-12 03:04:00 +0200

    Sync

--- src/DG.c
+++ src/DG.c
@@ -10,9 +10,30 @@ typedef struct {
 DG_t *DG_create(emap_surface_t *es, emap_float dE)
 {
         SB_t  *sb;
-        size_t sbcount;
+        register size_t sbcount, i;
 
-        
+        emap_float El, Eh; /**< energy band boundaries - low, high */
+
+        sbcount = es->mcount;
+        sb      = alloc_array(SB_t, sbcount);
+
+#pragma omp parallel for
+        for (i = 0; i < sbcount; ++i) {
+                sb[i].spoint = es->mpoint[i];
+                sb[i].dgnode = alloc_type(struct DG_node);
+                sb[i].dgnode->child = NULL;
+                sb[i].dgnode->count = i;
+                sb[i].dgnode->point = es->mpoint[i];
+        }
+
+        El = sb[0].spoint->cmaximum->y;
+        Eh = El + dE;
+
+        while (sbcount > 1) {
+                /*
+                 * 
+                 */
+        }
 
         return NULL;
 }
